7a3877b4c0e2760f7f0bec7e7c0ae4411e3e90cf
EastSmith <67545545+EastSmith@users.noreply.github.com>
2021-03-21 20:19:02 +0800

    Update policy_value_net_paddlepaddle.py

--- policy_value_net_paddlepaddle.py
+++ policy_value_net_paddlepaddle.py
@@ -1,77 +1,75 @@
-import paddle.fluid as fluid
+import paddle
 import numpy as np
-from paddle.fluid.param_attr import ParamAttr
-from paddle.fluid.layer_helper import LayerHelper
-from paddle.fluid.dygraph import Conv2D, Linear
+
+import paddle.nn as nn 
+import paddle.nn.functional as F
 
 
 
-class Net(fluid.dygraph.Layer):
+class Net(paddle.nn.Layer):
     def __init__(self,board_width, board_height):
         super(Net, self).__init__()
         self.board_width = board_width
         self.board_height = board_height
-        # common layers
-        self.conv1 = Conv2D(num_channels=4,num_filters=32,filter_size=3,padding=1,act="relu")
-        self.conv2 = Conv2D(num_channels=32,num_filters=64,filter_size=3,padding=1,act="relu")
-        self.conv3 = Conv2D(num_channels=64,num_filters=128,filter_size=3,padding=1,act="relu")
-        # action policy layers
-        self.act_conv1 = Conv2D(num_channels=128,num_filters=4,filter_size=1,padding=0,act="relu")
-        self.act_fc1 = Linear(4*self.board_width*self.board_height,
+        # 公共网络层
+        self.conv1 = nn.Conv2D(in_channels=4,out_channels=32,kernel_size=3,padding=1)
+        self.conv2 = nn.Conv2D(in_channels=32,out_channels=64,kernel_size=3,padding=1)
+        self.conv3 = nn.Conv2D(in_channels=64,out_channels=128,kernel_size=3,padding=1)
+        # 行动策略网络层
+        self.act_conv1 = nn.Conv2D(in_channels=128,out_channels=4,kernel_size=1,padding=0)
+        self.act_fc1 = nn.Linear(4*self.board_width*self.board_height,
                                  self.board_width*self.board_height)
-        self.val_conv1 = Conv2D(num_channels=128,num_filters=2,filter_size=1,padding=0,act="relu")
-        self.val_fc1 = Linear(2*self.board_width*self.board_height, 64,act="relu")
-        self.val_fc2 = Linear(64, 1,act="tanh")
+        self.val_conv1 = nn.Conv2D(in_channels=128,out_channels=2,kernel_size=1,padding=0)
+        self.val_fc1 = nn.Linear(2*self.board_width*self.board_height, 64)
+        self.val_fc2 = nn.Linear(64, 1)
 
     def forward(self, inputs):
-        # common layers 
-        x = self.conv1(inputs)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        # action policy layers
-        x_act = self.act_conv1(x)
-        x_act = fluid.layers.reshape(
+        # 公共网络层 
+        x = F.relu(self.conv1(inputs))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        # 行动策略网络层
+        x_act = F.relu(self.act_conv1(x))
+        x_act = paddle.reshape(
                 x_act, [-1, 4 * self.board_height * self.board_width])
-        x_act = self.act_fc1(x_act)
-        x_act  = fluid.layers.log(fluid.layers.softmax(x_act))        
-        # state value layers
-        x_val  = self.val_conv1(x)
-        x_val = fluid.layers.reshape(
+        
+        x_act  = F.log_softmax(self.act_fc1(x_act))        
+        # 状态价值网络层
+        x_val  = F.relu(self.val_conv1(x))
+        x_val = paddle.reshape(
                 x_val, [-1, 2 * self.board_height * self.board_width])
-        x_val = self.val_fc1(x_val)
-        x_val = self.val_fc2(x_val)
+        x_val = F.relu(self.val_fc1(x_val))
+        x_val = F.tanh(self.val_fc2(x_val))
 
         return x_act,x_val
 
 class PolicyValueNet():
-    """policy-value network """
+    """策略&值网络 """
     def __init__(self, board_width, board_height,
                  model_file=None, use_gpu=True):
         self.use_gpu = use_gpu
         self.board_width = board_width
         self.board_height = board_height
-        self.l2_const = 1e-4  # coef of l2 penalty
-        # the policy value net module
-        # place = fluid.CUDAPlace(0) if use_gpu else fluid.CPUPlace()
+        self.l2_const = 1e-3  # coef of l2 penalty
+        
 
         self.policy_value_net = Net(self.board_width, self.board_height)        
         
-        self.optimizer  = fluid.optimizer.Adam(learning_rate=0.02,
-                                parameter_list=self.policy_value_net.parameters(),
-                                regularization= fluid.regularizer.L2Decay(regularization_coeff=0.001)
-                                     )
+        self.optimizer  = paddle.optimizer.Adam(learning_rate=0.02,
+                                parameters=self.policy_value_net.parameters(), weight_decay=self.l2_const)
+                                     
 
         if model_file:
-            net_params, _ = fluid.dygraph.load_dygraph(model_file)
-            self.policy_value_net.load_dict(net_params)
+            net_params = paddle.load(model_file)
+            self.policy_value_net.set_state_dict(net_params)
             
     def policy_value(self, state_batch):
         """
         input: a batch of states
         output: a batch of action probabilities and state values
         """
-        # state_batch = fluid.dygraph.to_variable(np.ndarray(state_batch))
-        state_batch = fluid.dygraph.to_variable(state_batch)
+        # state_batch = paddle.to_tensor(np.ndarray(state_batch))
+        state_batch = paddle.to_tensor(state_batch)
         log_act_probs, value = self.policy_value_net(state_batch)
         act_probs = np.exp(log_act_probs.numpy())
         return act_probs, value.numpy()
@@ -87,7 +85,7 @@ class PolicyValueNet():
                 -1, 4, self.board_width, self.board_height)).astype("float32")
         
         # print(current_state.shape)
-        current_state = fluid.dygraph.to_variable(current_state)
+        current_state = paddle.to_tensor(current_state)
         log_act_probs, value = self.policy_value_net(current_state)
         act_probs = np.exp(log_act_probs.numpy().flatten())
         
@@ -98,32 +96,31 @@ class PolicyValueNet():
     def train_step(self, state_batch, mcts_probs, winner_batch, lr=0.002):
         """perform a training step"""
         # wrap in Variable
-        state_batch = fluid.dygraph.to_variable(state_batch)
-        mcts_probs = fluid.dygraph.to_variable(mcts_probs)
-        winner_batch = fluid.dygraph.to_variable(winner_batch)
+        state_batch = paddle.to_tensor(state_batch)
+        mcts_probs = paddle.to_tensor(mcts_probs)
+        winner_batch = paddle.to_tensor(winner_batch)
 
         # zero the parameter gradients
         self.optimizer.clear_gradients()
         # set learning rate
-        self.optimizer  = fluid.optimizer.Adam(lr,
-                                parameter_list=self.policy_value_net.parameters(),
-                                regularization= fluid.regularizer.L2Decay(regularization_coeff=0.001)
-                                     )
+        self.optimizer.set_lr(lr)
+
+                                     
 
         # forward
         log_act_probs, value = self.policy_value_net(state_batch)
         # define the loss = (z - v)^2 - pi^T * log(p) + c||theta||^2
         # Note: the L2 penalty is incorporated in optimizer
-        value = fluid.layers.reshape(x=value, shape=[-1])
-        value_loss = fluid.layers.mse_loss(input=value, label=winner_batch)
-        policy_loss = -fluid.layers.reduce_mean(fluid.layers.reduce_sum(mcts_probs*log_act_probs, dim=1))
+        value = paddle.reshape(x=value, shape=[-1])
+        value_loss = F.mse_loss(input=value, label=winner_batch)
+        policy_loss = -paddle.mean(paddle.sum(mcts_probs*log_act_probs, axis=1))
         loss = value_loss + policy_loss
         # backward and optimize
         loss.backward()
         self.optimizer.minimize(loss)
         # calc policy entropy, for monitoring only
-        entropy = -fluid.layers.reduce_mean(
-                fluid.layers.reduce_sum(fluid.layers.exp(log_act_probs) * log_act_probs, dim=1)
+        entropy = -paddle.mean(
+                paddle.sum(paddle.exp(log_act_probs) * log_act_probs, axis=1)
                 )
         return loss.numpy(), entropy.numpy()[0]    
 
@@ -134,19 +131,4 @@ class PolicyValueNet():
     def save_model(self, model_file):
         """ save model params to file """
         net_params = self.get_policy_param()  # get model params
-        fluid.save_dygraph(net_params, model_file)
-
-# if __name__ == '__main__':
-#     with fluid.dygraph.guard():
-#         img = np.random.rand(3, 4, 15, 15).astype("float32")
-#         # img = fluid.dygraph.to_variable(img)
-#         outs = PolicyValueNet(15,15).policy_value(img)[1]
-#         print(outs.shape,outs)
-
-#         x1 = np.random.rand(3, 4, 15, 15).astype("float32")
-#         x2 = np.random.rand(3, 225).astype("float32")
-#         x3 = np.random.rand(3).astype("float32")
-
-        
-#         e = PolicyValueNet(15,15).train_step(x1,x2,x3)[1]
-#         print(e,e.shape)
+        paddle.save(net_params, model_file)
